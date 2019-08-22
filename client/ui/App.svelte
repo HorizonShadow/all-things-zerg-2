2e6c4fb544add6b0d27cfb7f46dbea0e0433f3d9
Joshua LeBlanc <joshleblanc94@gmail.com>
2019-08-22 11:46:25 +0000

    started import functionality

--- client/ui/App.svelte
+++ client/ui/App.svelte
@@ -1,6 +1,7 @@
 <script>
     import Navbar from './components/Navbar';  
     import Scouting from './pages/Scouting';
+    import Import from './pages/Import';
     import { Router, Route } from 'svelte-routing';
 
 </script>
@@ -9,5 +10,6 @@
     <Navbar />
     <div class="container">
         <Route path="/scouting" component={Scouting} />
+        <Route path="/import" component={Import} />
     </div>
 </Router>
---
+++ client/ui/pages/Import.svelte
@@ -0,0 +1,11 @@
+<div class="columns">
+    <div class="column">
+        <textarea class="textarea">
+
+        </textarea>
+        <button class="button">Submit</button>
+    </div>
+</div>
+
+
+

ca0023cd0dc5716413de8458ea72a814a9016edc
Joshua LeBlanc <joshleblanc94@gmail.com>
2019-08-23 10:54:13 +0000

    working on imports

--- client/ui/pages/Import.svelte
+++ client/ui/pages/Import.svelte
@@ -1,9 +1,28 @@
+<script>
+    import { Build } from '/lib/Build';
+    let name = "";
+    let buildOrder = "";
+    let build = new Build();
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        build.import(name, buildOrder);
+    }
+</script>
+
 <div class="columns">
     <div class="column">
-        <textarea class="textarea">
-
-        </textarea>
-        <button class="button">Submit</button>
+        <form on:submit={handleSubmit}>
+            <div class="field">
+                <label class="label">Name</label>
+                <input class="input" bind:value={name} /> 
+            </div>
+            <div class="field">
+                <label class="label">Spawning Tool Build</label>
+                <textarea class="textarea" bind:value={buildOrder}></textarea>
+            </div>
+            <button class="button" type="submit">Submit</button>
+        </form>
     </div>
 </div>
 
